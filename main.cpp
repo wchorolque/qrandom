16cbe67d78d4a4f69f3c23fa88768be0dd284331
wchorolque <wchorolque@gmail.com>
2012-12-25 02:57:22 -0400

    Agregated the generation of random values

--- main.cpp
+++ main.cpp
@@ -8,8 +8,9 @@ int main (int argc, char* argv[])
     QApplication app(argc, argv);
     QTextStream cout(stdout);
     bool ok;
-
-    int seed = QInputDialog::getInt(
+    int seed;
+    int answer;
+    seed = QInputDialog::getInt(
                 0,                          // Parent
                 "Seed for random numbers",  // Title
                 "Enter the seed:",          // Label
@@ -21,7 +22,18 @@ int main (int argc, char* argv[])
                 );
     srand(seed);
     if (true == ok) {
-        cout << rand() << endl;
+        do {
+            qint64 nextRandomValue = rand();
+            QString response = QString("The next random value is: %1\n%2")
+                    .arg(nextRandomValue)
+                    .arg("Do you want to generate another value?");
+            answer = QMessageBox::question(
+                        0,                  // Parent
+                        "New random value", // Title
+                        response,           // Label
+                        QMessageBox::Yes | QMessageBox::No
+                        );
+        } while (QMessageBox::Yes == answer);
     }
 
     return EXIT_SUCCESS;
